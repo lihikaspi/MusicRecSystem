efa880519dff54de44910bb9a6c5c8a2e54fbc39
lihikaspi <128523042+lihikaspi@users.noreply.github.com>
2025-09-11 13:36:44 +0300

    add edge_type mapping to config file

--- config.py
+++ config.py
@@ -5,3 +5,12 @@ DATASET_TYPE = "flat" # options: "flat", "sequential"
 # save directories
 DATA_DIR = f"final_project/project_data/YambdaData{DATASET_SIZE}/"
 PROCESSED_DIR = "final_project/processed_data/"
+
+# Mapping edge type names to integer IDs
+EDGE_TYPE_MAPPING = {
+    "listen": 0,
+    "like": 1,
+    "dislike": 2,
+    "unlike": 3,
+    "undislike": 4
+}
